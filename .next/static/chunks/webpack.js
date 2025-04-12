05f66c39e604b61f0330e961796f7ae8647f1c58
HO YUEN <40999973-alecho26@users.noreply.replit.com>
2025-04-12 17:16:17 +0000

    修復了 Footer.tsx 中的 React 導入問題

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -181,7 +181,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("64881ae227c1aa39")
+/******/ 		__webpack_require__.h = () => ("4132b33e7c76d204")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
